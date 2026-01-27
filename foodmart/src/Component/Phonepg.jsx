2546d380bf9613847fb4be579a70219cb846a143
pransusojitra <pransusojitra1@gmail.com>
2026-01-27 18:07:21 +0530

    Add new

--- foodmart/src/Component/Phonepg.jsx
+++ foodmart/src/Component/Phonepg.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 import "./Phonepg.css";
-import photo from '../assets/phone.png'
+import phone from "../assets/phone.png";
+import appStore from "../assets/app-store.jpg";
+import googlePlay from "../assets/google-play.jpg";
 
 const Phonepg = () => {
     return (
+        <div className="p-3">
+            <div className="rounded-5" style={{ backgroundColor: "#fff9eb" }}>
+                <div className="row align-items-center">
 
-        <div className="container"style={{ backgroundColor: "#fff9eb" }}>
-            <div className="row">
-                <div className="col-4">
-                    <img src={photo} />
+                    <div className="col-lg-4 col-md-4 col-12 text-center">
+                        <img src={phone} alt="Phone App" className="img-fluid" />
+                    </div>
 
-                </div>
+                    <div className="col-lg-8 col-md-8 col-12 d-flex flex-column justify-content-center align-items-center text-center">
+                        <h1 className="fw-bold mb-3">
+                            Shop faster with Foodmart App
+                        </h1>
 
-                    <div className="col-8 d-flex flex-column justify-content-center align-items-center">
-                        <h1>Shop faster with foodmart App</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis sed ptibus liberolectus nonet psryroin. Amet sed lorem posuere sit iaculis amet, ac urna. Adipiscing fames semper erat ac in suspendisse iaculis. Amet blandit tortor praesent ante vitae. A, enim pretiummi senectus magna. Sagittis sed ptibus liberolectus non et psryroin.</p>
+                        <p className="container text-muted mb-4 w-lg-75 w-md-75 w-100">
+                            Fresh groceries are now just a tap away with the Foodmart mobile app. Explore trending products, seasonal essentials, and daily needs all in one place. Enjoy fast and reliable doorstep delivery that saves your time and effort. The app is designed for smooth browsing and easy ordering. Get exclusive deals, special discounts, and smart recommendations. Foodmart makes everyday shopping simple, quick, and convenient.
+                        </p>
+
+                        <div className="d-flex gap-3 flex-wrap justify-content-center">
+                            <img src={appStore} alt="App Store" className="store-btn" />
+                            <img src={googlePlay} alt="Google Play" className="store-btn" />
+                        </div>
                     </div>
                 </div>
             </div>
-
+        </div>
     );
 };
 
